98ca953ca7905e800181bad3d77aa50fade21e2d
yu-Celik <celik.yus@outlook.com>
2024-04-02 19:37:01 +0200

    correct

--- frontend/src/config/axiosConfig.ts
+++ frontend/src/config/axiosConfig.ts
@@ -5,7 +5,7 @@ axios.defaults.withCredentials = true;
 
 // Vous pouvez également configurer d'autres paramètres par défaut ici
 // Par exemple, définir une URL de base pour toutes les requêtes
-axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+axios.defaults.baseURL = "https://chat-app-teamate.onrender.com/api";
 
 // Exporter l'instance Axios configurée pour une utilisation dans toute l'application
 export default axios;
