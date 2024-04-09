ff37c04418611de2f17f2c58dd95b5f5ea4f581f
yu-Celik <celik.yus@outlook.com>
2024-04-09 05:19:08 +0200

    commit 09/04 new

--- frontend/src/components/ui/infinite-moving-cards.tsx
+++ frontend/src/components/ui/infinite-moving-cards.tsx
@@ -34,21 +34,24 @@ export const InfiniteMovingCards = ({
 
    const [start, setStart] = useState(false);
    function addAnimation() {
-      if (containerRef.current && scrollerRef.current) {
-         const scrollerContent = Array.from(scrollerRef.current.children);
+      requestAnimationFrame(() => {
+         if (containerRef.current && scrollerRef.current) {
+            const scrollerContent = Array.from(scrollerRef.current.children);
 
-         scrollerContent.forEach((item) => {
-            const duplicatedItem = item.cloneNode(true);
-            if (scrollerRef.current) {
-               scrollerRef.current.appendChild(duplicatedItem);
-            }
-         });
+            scrollerContent.forEach((item) => {
+               const duplicatedItem = item.cloneNode(true);
+               if (scrollerRef.current) {
+                  scrollerRef.current.appendChild(duplicatedItem);
+               }
+            });
 
-         getDirection();
-         getSpeed();
-         setStart(true);
-      }
+            getDirection();
+            getSpeed();
+            setStart(true);
+         }
+      });
    }
+
    const getDirection = () => {
       if (containerRef.current) {
          if (direction === "left") {
