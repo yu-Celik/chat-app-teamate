26c67c4681d8145b09016002860f7a9911200097
yu-Celik <celik.yus@outlook.com>
2024-04-13 23:18:36 +0200

    deploy

--- frontend/src/config/axiosConfig.ts
+++ frontend/src/config/axiosConfig.ts
@@ -5,8 +5,8 @@ axios.defaults.withCredentials = true;
 
 // Vous pouvez également configurer d'autres paramètres par défaut ici
 // Par exemple, définir une URL de base pour toutes les requêtes
-axios.defaults.baseURL = "https://chat-app-teamate.onrender.com/api"; // Pour le site chat-app-teamate.onrender.com
-// axios.defaults.baseURL = "http://192.168.1.103:5000/api"; // Pour le site en local
+// axios.defaults.baseURL = "https://chat-app-teamate.onrender.com/api"; // Pour le site chat-app-teamate.onrender.com
+axios.defaults.baseURL = "http://192.168.1.103:5000/api"; // Pour le site en local
 
 // Exporter l'instance Axios configurée pour une utilisation dans toute l'application
 export default axios;
