9ec02523177310fc3c950d851170571a162738bc
yu-Celik <celik.yus@outlook.com>
2024-04-17 14:19:21 +0200

    commit 17/04

---
+++ frontend/src/pages/NotFoundPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+function NotFoundPage() {
+    return (
+        <Container>
+            <Typography variant="h1">404 Page Not Found</Typography>
+            <Typography>We're sorry, the page you requested could not be found.</Typography>
+            <Button variant="contained" color="primary">Go Home</Button>
+        </Container>
+    );
+}
+
+export default NotFoundPage;
